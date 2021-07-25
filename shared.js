f18af9d720bd7ca518b69223fec9ccfd309e509f
AstakhovV <astakhclever@gmail.com>
2021-07-25 15:24:18 +0500

    Final fix, sass

--- shared.js
+++ shared.js
@@ -47,3 +47,56 @@ toggleButton.addEventListener('click', () => {
         backdrop.classList.add('open')
     }, 10)
 })
+
+// animation show
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('show-element');
+        }
+    });
+}
+let options = { threshold: 0.02 };
+const observer = new IntersectionObserver(onEntry, options);
+const elements = document.querySelectorAll('.package');
+const elementsMain = document.querySelectorAll('.plan');
+const elementsKeyFeature = document.querySelectorAll('.key-features__list li');
+
+for (let elm of elements) {
+    observer.observe(elm);
+}
+for (let elm of elementsMain) {
+    observer.observe(elm);
+}
+for (let elm of elementsKeyFeature) {
+    observer.observe(elm);
+}
+// show maximize image
+
+const imagesPreview = document.querySelectorAll('.popupbox_preview')
+const popupbox = document.querySelector('.popupbox')
+const bodyBox = document.querySelector('body')
+const popupbox_header = document.querySelector('.popupbox_header')
+const popupbox_image = document.querySelector('.popupbox_image')
+const closeImage = document.querySelector('[title="Закрыть"]')
+imagesPreview.forEach(image => image.addEventListener('click', (e) => {
+    backdrop.style.display = 'block'
+    setTimeout(()=>{
+        backdrop.classList.add('open')
+    }, 10)
+    bodyBox.style.overflow = 'hidden'
+    popupbox.style.display = 'block'
+    popupbox_header.firstChild.data = image.closest('section').childNodes[1].children[0].innerText
+    popupbox_image.children[0].alt = image.children[0].alt
+    popupbox_image.children[0].src = image.children[0].alt + '.jpg'
+
+}))
+
+if (closeImage) {
+    closeImage.addEventListener('click', () => {
+        popupbox.style.display = 'none'
+        bodyBox.style.overflow = ''
+        closeModal()
+    })
+}
